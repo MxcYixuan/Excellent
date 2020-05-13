827132f59f98ad2701e267d1a84087cb5760f925
MxcYixuan <xiaowei7002@126.com>
2020-05-13 14:58:24 +0800

    Create quick_sort.cpp

---
+++ Data_Structure_And_Algorithm/quick_sort.cpp
@@ -0,0 +1,23 @@
+int partition_nums(vector<int> &nums, int left, int right) {
+    int base = nums[left];
+    int i = left ;
+    int j = right;
+    while(i != j) {
+        while(nums[j] >= base && i < j) j--;
+        while(nums[i] <= base && i < j) i++;
+        if (i < j) {
+            swap(nums[i], nums[j]);
+        }
+    }
+
+    swap(nums[left], nums[j]);
+    return j;
+}
+
+void quickSort(vector<int> &nums, int left, int right) {
+    if (left > right) return;
+    int mid = partition_nums(nums, left, right);
+    quickSort(nums, left, mid - 1);
+    quickSort(nums, mid + 1, right);
+
+}
