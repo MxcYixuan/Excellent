613eb7778c5373e808d64a9aef4a61f7ae601f48
MxcYixuan <xiaowei7002@126.com>
2020-05-15 10:37:38 +0800

    Update PeekingIterator.cpp
    顶端迭代器peek实现

--- Data_Structure_And_Algorithm/PeekingIterator.cpp
+++ Data_Structure_And_Algorithm/PeekingIterator.cpp
@@ -1,1 +1,72 @@
-//顶端迭代器
+/*给定一个迭代器类的接口，接口包含两个方法： next() 和 hasNext()。
+设计并实现一个支持 peek() 操作的顶端迭代器 -- 其本质就是把原本应由 next() 方法返回的元素 peek() 出来。
+
+示例:
+假设迭代器被初始化为列表 [1,2,3]。
+调用 next() 返回 1，得到列表中的第一个元素。
+现在调用 peek() 返回 2，下一个元素。在此之后调用 next() 仍然返回 2。
+最后一次调用 next() 返回 3，末尾元素。在此之后调用 hasNext() 应该返回 false。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/peeking-iterator
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+    int curr;
+    bool end_flag;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    end_flag = false;
+        if (Iterator::hasNext()) {
+            curr = Iterator::next();
+        } else {
+            end_flag = true;
+        }
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return curr;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    int res;
+        if (end_flag) return -1;
+        res = curr;
+        if (Iterator::hasNext()) {
+            curr = Iterator::next();
+
+        } else {
+            end_flag = true;
+        }
+        return res;
+	}
+	
+	bool hasNext() const {
+	    return !end_flag;
+	}
+};
