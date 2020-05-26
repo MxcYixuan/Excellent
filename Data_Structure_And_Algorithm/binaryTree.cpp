eb685c946e2a9c8a273417d9c63a224863d1859f
MxcYixuan <xiaowei7002@126.com>
2020-05-26 19:34:36 +0800

    Update binaryTree.cpp

--- Data_Structure_And_Algorithm/binaryTree.cpp
+++ Data_Structure_And_Algorithm/binaryTree.cpp
@@ -40,3 +40,26 @@ vector<ListNode*> listOfDepth(TreeNode* tree) {
         }
         return res;
     }
+
+//1367. 二叉树中的列表
+/*给你一棵以 root 为根的二叉树和一个 head 为第一个节点的链表。
+如果在二叉树中，存在一条一直向下的路径，且每个点的数值恰好一一对应以 head 为首的链表中每个节点的值，
+那么请你返回 True ，否则返回 False 。
+一直向下的路径的意思是：从树中某个节点开始，一直连续向下的路径。
+*/
+bool isSubPath(ListNode* head, TreeNode* root) {
+        //DFS遍历数
+        if (!root) return false;
+        bool result1 = dfs(head, root);
+        return result1 || isSubPath(head, root->left) 
+                       || isSubPath(head, root->right);
+    }
+
+    bool dfs(ListNode* head, TreeNode* root) {
+        if (!head) return true;
+        if (!root) return false;
+        if (head->val != root->val) return false;
+        return dfs(head->next, root->left) 
+            || dfs(head->next, root->right);
+    }
+
