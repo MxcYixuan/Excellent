f7f56bead9064e6dbbab0633409a7b8cd2241d16
qisheng.cxw <qisheng.cxw@alibaba.com>
2020-06-06 22:48:21 +0800

    update merge two list

--- Data_Structure_And_Algorithm/ListOperator.cpp
+++ Data_Structure_And_Algorithm/ListOperator.cpp
@@ -20,3 +20,42 @@ ListNode* removeNthFromEnd(ListNode* head, int n) {
     } else head = head->next;
     return head;
 }
+
+
+//21. 合并两个有序链表
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//方法1 迭代，时间复杂度O(M+N),空间复杂度O(1)
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (!l1 && !l2) return nullptr;
+    if (!l1) return l2;
+    if (!l2) return l1;
+    ListNode* head = new ListNode(0);
+    ListNode* curr = head;
+    while(l1 && l2) {
+        if (l1->val < l2->val) {
+            curr->next = l1;
+            l1 = l1->next;
+        } else {
+            curr->next = l2;
+            l2 = l2->next;
+        }
+        curr = curr->next;
+    }
+    if (l1) curr->next = l1;
+    if (l2) curr->next = l2;
+    return head->next;
+}
+
+//方法2 递归，时间复杂度O(M+N),空间复杂度O(M+N)
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if(!l1) return l2;
+    if(!l2) return l1;
+    if(l1->val < l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
+
