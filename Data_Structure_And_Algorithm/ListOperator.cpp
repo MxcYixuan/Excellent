7afde03b6858d0377308d5f15e0975ed729a8e09
MxcYixuan <xiaowei7002@126.com>
2020-06-13 16:01:25 +0800

    Update ListOperator.cpp

--- Data_Structure_And_Algorithm/ListOperator.cpp
+++ Data_Structure_And_Algorithm/ListOperator.cpp
@@ -131,6 +131,30 @@ ListNode* mergeKLists(vector<ListNode*>& lists) {
     return merge(lists, 0, lists.size() - 1);
 }
 
-//
+//24. 两两交换链表中的节点
+/*给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+示例:
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+*/
+ListNode* swapPairs(ListNode* head) {    
+    if(!head) return nullptr;
+    if(!head->next) return head;
+    ListNode* pre = new ListNode(0);
+    ListNode* root = pre;
+    pre->next = head;
+    ListNode* cur = head;
+    ListNode* next = cur->next;
+    while (next && cur) {
+        cur->next = next->next;
+        pre->next = next;
+        pre->next->next = cur;     
+        pre = cur;
+        if (cur) cur = cur->next;
+        if (cur) next = cur->next;
+    }
+    return root->next;
+}
+
 
 
