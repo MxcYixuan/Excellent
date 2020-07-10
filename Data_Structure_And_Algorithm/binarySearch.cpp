3fa10c23542386cc9f617a5a3d9f5b19d8a7f184
MxcYixuan <xiaowei7002@126.com>
2020-07-10 16:52:04 +0800

    Update binarySearch.cpp

--- Data_Structure_And_Algorithm/binarySearch.cpp
+++ Data_Structure_And_Algorithm/binarySearch.cpp
@@ -17,3 +17,67 @@ int binarySearch(vector<int> &nums, int target) {
     }
     return low;
 }
+
+
+//二分查找，详细介绍
+https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/er-fen-cha-zhao-suan-fa-xi-jie-xiang-jie-by-labula/
+
+int binary_search(vector<int>& nums, int target) {
+    if (nums.size() == 0) return -1;
+    int left = 0, right = nums.size() - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] < target) {
+            //[mid+1, right]
+            left = mid + 1;
+        }else if(nums[mid] > target) {
+            //[left, mid-1]
+            right = mid - 1;
+        }else if(nums[mid] == target) {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int left_bound(vector<int>& nums, int target) {
+    if (nums.size() == 0) return -1;
+    int left = 0, right = nums.size() - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            //[mid+1, right]
+            left = mid + 1;
+        }else if (nums[mid] > target) {
+            //[left, mid - 1]
+            right = mid - 1;
+        } else if (nums[mid] == target){
+            // 别返回，收缩右侧边界
+            right = mid - 1;
+        }
+    }
+    if (left == nums.size() || nums[left] != target)
+        return -1;
+    return left;
+}
+
+int right_bound(vector<int>& nums, int target) {
+    if (nums.size() == 0) return -1;
+    int left = 0, right = nums.size() - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            //[mid + 1, right]
+            left = mid + 1;
+        }else if (nums[mid] > target) {
+            //[left, mid - 1]
+            right = mid - 1;
+        }else if (nums[mid] == target) {
+            // 别返回，收缩左侧边界
+            left = mid + 1;
+        }
+    }
+    if(right < 0 || nums[right] != target)
+        return -1;
+    return right;
+}
