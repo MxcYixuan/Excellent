566dea8487d99304582e28d36b6ba219d62ce526
MxcYixuan <xiaowei7002@126.com>
2020-07-11 23:33:52 +0800

    Update binarySearch.cpp

--- Data_Structure_And_Algorithm/binarySearch.cpp
+++ Data_Structure_And_Algorithm/binarySearch.cpp
@@ -333,6 +333,53 @@ bool search(vector<int>& nums, int target) {
     }
     return false;
 }
+//153. 寻找旋转排序数组中的最小值
+//难度中等
+/*假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+请找出其中最小的元素。
+你可以假设数组中不存在重复元素。
+*/
+int findMin(vector<int>& nums) {
+    int len = nums.size();
+    if (len == 1) return nums[0];
+    int left = 0, right = len - 1;
+
+    while(left < right) {
+        int mid = left + ((right - left) >> 1);
+        if (nums[mid] > nums[right])
+            left = mid + 1;
+        else if (nums[mid] < nums[right])
+            right = mid;
+        //else
+        //    right -= 1; 
+    }
+    return nums[left];
+}
+
+//154. 寻找旋转排序数组中的最小值 II
+/*假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+请找出其中最小的元素。
+注意数组中可能存在重复的元素。
+*/
+
+int findMin(vector<int>& nums) {
+    int len = nums.size();
+    if (len == 1) return nums[0];
+    int left = 0, right = len - 1;
+
+    while(left < right) {
+        int mid = left + ((right - left) >> 1);
+        if (nums[mid] > nums[right])
+            left = mid + 1;
+        else if (nums[mid] < nums[right])
+            right = mid;
+        else
+            right -= 1; 
+    }
+    return nums[left];
+}
 
 
 
