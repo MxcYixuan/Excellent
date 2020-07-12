ac570af54d807323600074d0864ec5fdc4a12354
MxcYixuan <xiaowei7002@126.com>
2020-07-12 22:11:36 +0800

    Update binarySearch.cpp

--- Data_Structure_And_Algorithm/binarySearch.cpp
+++ Data_Structure_And_Algorithm/binarySearch.cpp
@@ -401,5 +401,26 @@ int findPeakElement(vector<int>& nums) {
     return left;
 }
 
+//167. 两数之和 II - 输入有序数组
+//其实这道题解法是双指针法，但是二分查找也可以，二分查找是O(Nlog(N))
+//而双指针法是：O(N)
+/*给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+*/
+vector<int> twoSum(vector<int>& numbers, int target) {
+    int len = numbers.size();
+    int left = 0, right = len - 1;
+    while(left <= right) {
+        int sum = numbers[left] + numbers[right];
+        if (target == sum) {
+            return {left + 1, right + 1};
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return {-1, -1};
+}
 
 
