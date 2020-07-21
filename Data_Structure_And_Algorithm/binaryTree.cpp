ed2e9f2e8521d1160734c27268fd69de9cc087d9
MxcYixuan <xiaowei7002@126.com>
2020-07-21 22:25:01 +0800

    Update binaryTree.cpp

--- Data_Structure_And_Algorithm/binaryTree.cpp
+++ Data_Structure_And_Algorithm/binaryTree.cpp
@@ -77,3 +77,15 @@ TreeNode* trimBST(TreeNode* root, int L, int R) {
 
     }
 
+//112. 路径总和
+//给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+//说明: 叶子节点是指没有子节点的节点。
+bool hasPathSum(TreeNode* root, int sum) {
+    if (!root) return false;
+    if (!root->left && !root->right)
+        return sum == root->val;
+    return hasPathSum(root->left, sum - root->val) ||
+           hasPathSum(root->right, sum - root->val);
+}
+
+
