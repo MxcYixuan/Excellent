6fe84c6de438799f3905f2153e063bbb502850e0
MxcYixuan <xiaowei7002@126.com>
2020-07-21 21:59:12 +0800

    Update binarySearchTree.cpp

--- Data_Structure_And_Algorithm/binarySearchTree.cpp
+++ Data_Structure_And_Algorithm/binarySearchTree.cpp
@@ -252,3 +252,21 @@ TreeNode* dfs(vector<int>& nums, int left, int right) {
 TreeNode* sortedArrayToBST(vector<int>& nums) {
     return dfs(nums, 0, nums.size() - 1);
 }
+
+//110. 平衡二叉树
+//给定一个二叉树，判断它是否是高度平衡的二叉树。
+//本题中，一棵高度平衡二叉树定义为：
+//一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+int height(TreeNode* root) {
+    if(!root) return 0;
+    return 1 + max(height(root->left), height(root->right));
+}
+
+bool isBalanced(TreeNode* root) {
+    if (!root) return true;
+    int left = height(root->left);
+    int right = height(root->right);
+    if (abs(left - right) >= 2)
+        return false;
+    return isBalanced(root->left) && isBalanced(root->right);
+}
