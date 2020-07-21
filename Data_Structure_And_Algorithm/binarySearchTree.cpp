a8199cdac483c80c65fbcf125da54a96c36d9077
MxcYixuan <xiaowei7002@126.com>
2020-07-21 21:51:52 +0800

    Update binarySearchTree.cpp

--- Data_Structure_And_Algorithm/binarySearchTree.cpp
+++ Data_Structure_And_Algorithm/binarySearchTree.cpp
@@ -236,3 +236,19 @@ TreeNode* buildTree(vector<int>&inorder, vector<int>&postorder, unordered_map<in
     node->right = buildTree(inorder, postorder, umap, postLeft+index-inLeft,postRight-1, index+1,inRight);
     return node;
 }
+//108. 将有序数组转换为二叉搜索树
+//将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+//本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+TreeNode* dfs(vector<int>& nums, int left, int right) {
+    if (left > right)
+        return nullptr;
+    int mid = left + (right - left) / 2;
+    TreeNode* root = new TreeNode(nums[mid]);
+    root->left = dfs(nums, left, mid - 1);
+    root->right = dfs(nums, mid + 1, right);
+    return root;
+}
+
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return dfs(nums, 0, nums.size() - 1);
+}
