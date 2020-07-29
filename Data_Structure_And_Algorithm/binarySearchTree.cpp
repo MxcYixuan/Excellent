fafccd031ed6c5b90a737b25e33a2e57f1a3e91f
MxcYixuan <xiaowei7002@126.com>
2020-07-29 22:18:02 +0800

    Update binarySearchTree.cpp

--- Data_Structure_And_Algorithm/binarySearchTree.cpp
+++ Data_Structure_And_Algorithm/binarySearchTree.cpp
@@ -284,3 +284,52 @@ int minDepth(TreeNode* root) {
 }
 
 
+//173. 二叉搜索树迭代器
+//实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+//调用 next() 将返回二叉搜索树中的下一个最小的数。
+      7
+   3    15
+      9    20
+/*BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // 返回 3
+iterator.next();    // 返回 7
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 9
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 15
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 20
+iterator.hasNext(); // 返回 false
+*/
+//next() 和 hasNext() 操作的时间复杂度是 O(1)，并使用 O(h) 内存，其中 h 是树的高度。
+//你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 中至少存在一个下一个最小的数。
+
+   
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+    BSTIterator(TreeNode* root) {
+        while(root) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+        int val = node->val;
+        node = node->right;
+        while(node) {
+            st.push(node);
+            node = node->left;
+        }
+        return val;
+    }
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+       return !st.empty();
+    }
+};
+
+
