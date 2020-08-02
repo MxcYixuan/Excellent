133c2dbc77fa0cdf09e6bd8a8be5f388b499c8d5
MxcYixuan <xiaowei7002@126.com>
2020-08-02 11:46:51 +0800

    Update dfs.cpp

--- Data_Structure_And_Algorithm/dfs.cpp
+++ Data_Structure_And_Algorithm/dfs.cpp
@@ -46,3 +46,60 @@ Node* cloneGraph2(Node* node) {
     }
     return cloneNode;
 }
+
+//200. 岛屿数量
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+此外，你可以假设该网格的四条边均被水包围。
+
+示例 1:
+
+输入:
+[
+['1','1','1','1','0'],
+['1','1','0','1','0'],
+['1','1','0','0','0'],
+['0','0','0','0','0']
+]
+输出: 1
+示例 2:
+
+输入:
+[
+['1','1','0','0','0'],
+['1','1','0','0','0'],
+['0','0','1','0','0'],
+['0','0','0','1','1']
+]
+输出: 3
+解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+void dfs(vector<vector<char>>& grid, int i, int j) {
+    int m = grid.size();
+    int n = grid[0].size();
+    grid[i][j] = '0';
+    if (i - 1 >= 0 && grid[i - 1][j] == '1') dfs(grid, i - 1, j);
+    if (i + 1 < m && grid[i + 1][j] == '1') dfs(grid, i + 1, j);
+    if (j - 1 >= 0 && grid[i][j - 1] == '1') dfs(grid, i, j - 1);
+    if (j + 1 < n && grid[i][j + 1] == '1') dfs(grid, i, j + 1);
+
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    int num_lands = 0;
+    int m = grid.size();
+    if(!m) return 0;
+    int n = grid[0].size();
+    if (!n) return 0;
+
+
+    for (int i = 0; i != m; i++) {
+        for (int j = 0; j != n; j++) {
+            if (grid[i][j] == '1') {
+                ++num_lands;
+                dfs(grid, i, j);
+            }
+        }
+    }
+    return num_lands;
+}
+
