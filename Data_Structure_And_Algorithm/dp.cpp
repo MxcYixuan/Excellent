60f443b3bfe6cdf5635bf58679a2194ca0af90cf
MxcYixuan <xiaowei7002@126.com>
2020-08-30 11:59:23 +0800

    Update dp.cpp

--- Data_Structure_And_Algorithm/dp.cpp
+++ Data_Structure_And_Algorithm/dp.cpp
@@ -283,4 +283,32 @@ int fib(int N) {
     }
     return sum;
 }
-
+//322. 零钱兑换
+/*给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+示例 1:
+输入: coins = [1, 2, 5], amount = 11
+输出: 3 
+解释: 11 = 5 + 5 + 1
+*/
+    
+//方法一、搜索回溯 [超出时间限制]
+int coinChange(int idx, vector<int>& coins, int amount) {
+    if (amount == 0) return 0;
+    if (idx < coins.size() && amount > 0) {
+        int numCoins = amount / coins[idx];
+        //cout << "numCoins: " << numCoins << endl;
+        int minNum = INT_MAX;
+        for (int i = 0; i <= numCoins; i++) {
+            int cur = i * coins[idx];
+            if (amount >= cur) {
+                int res = coinChange(idx + 1, coins, amount - cur);
+                if (res != -1) minNum = min(minNum, res + i);
+            }
+        }
+        return minNum == INT_MAX ? -1 : minNum;
+    }
+    return -1;
+}
+int coinChange(vector<int>& coins, int amount) {
+    return coinChange(0, coins, amount);
+}
