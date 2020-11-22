a3ba864d44192570e2373762e050eefc8adb5ded
MxcYixuan <xiaowei7002@126.com>
2020-11-22 15:14:20 +0800

    update k个一组翻转链表

--- Data_Structure_And_Algorithm/ListNodeOpt.cpp
+++ Data_Structure_And_Algorithm/ListNodeOpt.cpp
@@ -92,3 +92,104 @@ ListNode* reverseBetween(ListNode* head, int m, int n) {
     }
     return dump->next;
 }
+
+//25. K 个一组翻转链表
+/***
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+k 是一个正整数，它的值小于或等于链表的长度。
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+示例：
+给你这个链表：1->2->3->4->5
+当 k = 2 时，应当返回: 2->1->4->3->5
+当 k = 3 时，应当返回: 3->2->1->4->5
+***/
+//方法1：迭代+递归
+// 首先定义反转链表的操作
+ListNode* reverse(ListNode* node) {
+    if (!node) return node;
+    ListNode* pre = nullptr;
+    ListNode* curr = node;
+    ListNode* next = node;
+    while (curr != nullptr) {
+        next = curr->next;
+        curr->next = pre;
+        pre = curr;
+        curr = next;
+    }
+    return pre;
+}
+// 反转区间 [a, b) 的元素，注意是左闭右开 
+ListNode* reverse(ListNode* a, ListNode* b) {
+    ListNode* pre = nullptr;
+    ListNode* curr = a;
+    ListNode* next = a;
+    while (curr != b) {
+        next = curr->next;
+        curr->next = pre;
+        pre = curr;
+        curr = next;
+    }
+    return pre;
+}
+
+ListNode* reverseKGroup2(ListNode* head, int k) {
+    if (!head || !head->next) return head;
+    // 区间 [a, b) 包含 k 个待反转元素
+    ListNode* a;
+    ListNode* b;
+    a = b = head;
+    for (int i = 0; i != k; i++) {
+        // 不足 k 个，不需要反转，base case
+        if (!b) return head;
+        b = b->next;
+    }
+    // 反转前 k 个元素
+    ListNode* newNode = reverse(a, b);
+    // 递归反转后续链表并连接起来
+    a->next = reverseKGroup2(b, k);
+    return newNode;
+}
+方法2: 纯迭代做法
+// 翻转 head 与tail 之间的node
+pair<ListNode*, ListNode*> myReverse(ListNode* head, ListNode* tail) {
+    // 先找到下游节点，为了让翻转的链表可以指向下游
+    ListNode* pre = tail->next;
+    ListNode* curr = head;
+    ListNode* next = head;
+    while(pre != tail) {
+        next = curr->next;
+        curr->next = pre;
+        pre = curr;
+        curr = next;
+    }
+    // 返回翻转后的头结点和尾结点
+    return {tail, head};
+}
+// 方法2：以下是纯迭代方法，需要细品
+ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode* dump = new ListNode(0);
+    dump->next = head;
+    ListNode* pre = dump;
+    while(head != nullptr) {
+        ListNode* tail = pre;
+        for (int i = 0; i < k; i++) {
+            tail = tail->next;
+            if(!tail) return dump->next;
+        }
+        // 定义next节点，主要是为了连接 后驱节点
+        ListNode* next = tail->next;
+        pair<ListNode*, ListNode*> result = myReverse(head, tail);
+        head = result.first;
+        tail = result.second;
+        // 翻转之后，头结点需要设置
+        pre->next = head;
+        // 翻转之后，尾结点需要设置
+        tail->next = next;
+        // 更新pre节点 为tail
+        pre = tail;
+        // 更新head节点 为tail->next
+        head = tail->next;
+    }
+    return dump->next;
+}
