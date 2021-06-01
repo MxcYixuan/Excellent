87a61322799d90d1c595de936e3017f0070d3848
MxcYixuan <xiaowei7002@126.com>
2021-06-01 17:46:54 +0800

    Update subset_combine_permutate.cpp

--- Algorithm_Classifier/subset_combine_permutate.cpp
+++ Algorithm_Classifier/subset_combine_permutate.cpp
@@ -54,3 +54,28 @@ vector<vector<int>> subsets2(vector<int>& nums) {
     return res;
 }
 
+
+// 组合问题
+vector<vector<int>> res;
+void backtrack(int n, int k, int start, vector<int>& track) {
+    // 到达树的底部
+    if(k == track.size()) {
+        res.push_back(track);
+        return;
+    }
+    // 注意i 从 start 开始递增
+    for(int i = start; i <= n; i++) {
+        // 做选择
+        track.push_back(i);
+        backtrack(n, k, i + 1, track);
+        // 撤销选择
+        track.pop_back();
+    }
+}
+
+vector<vector<int>> combine(int n, int k) {
+    if(k <= 0 || n <= 0) return res;
+    vector<int> track;
+    backtrack(n, k, 1, track);
+    return res;
+}
